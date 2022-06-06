b2d8db9ec25bf5f2e168907af37c8707fe2fd165
Alex123-G <alexgallozo521@gmial.com>
2022-06-06 11:03:18 -0600

    Agregando funcionalidad mostrar propiedades favoritas

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,9 +18,17 @@ const Header = () => {
 					Mis favoritos
 				</span>
 			</div>
-			{/* {favorite_list.map(favorite => (
-				<div key={favorite.id}>{favorite.dimensiones}</div>
-			))} */}
+			{ishidden ? (
+				<div className="container-favorite-list">
+					{favorite_list.map(favorite => (
+						<div key={favorite.id} className="favorite-item">
+							<img className="favorite-item-img" src={favorite.url_img} alt={favorite.description} />
+						</div>
+					))}
+				</div>
+			) : (
+				""
+			)}
 		</header>
 	);
 };
